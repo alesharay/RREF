bc102064f02d9e873f63883c458080edaf4df3cc
Alesha Ray <alesha.ray@g.austincc.edu>
2020-05-01 22:27:12 -0500

    add working updates. Should be in better named branch

--- src/main/java/com/aleshamray/rref/Matrix.java
+++ src/main/java/com/aleshamray/rref/Matrix.java
@@ -1,18 +1,30 @@
 package com.aleshamray.rref;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.*;
 
 public class Matrix {
-  private List<Integer> matrix;
+  private ArrayList< ArrayList<Integer> > matrix;
   private int rowCount;
-  private List<Integer> rows;
   private int colCount;
-  private List<Integer> cols;
 
-  public Matrix(int rowCount, int colCount) {
-    setRowCount(rowCount);
-    setColCount(colCount);
-
+  public Matrix() {
+    matrix = new ArrayList< ArrayList<Integer> >();
+    rowCount = 3;
+    colCount = 3;
+    while(rowCount-- > 0) {
+      ArrayList<Integer> row = new ArrayList<Integer>();
+      int tmpColCount = colCount;
+      while(tmpColCount-- > 0) {
+        row.add(0);
+      }
+      matrix.add(row);
+    }
+  }
+  
+  public Matrix(int rowCount, int colCount, ArrayList<ArrayList<Integer>> matrix) {
+    updateMatrix(rowCount, colCount, matrix);
   }
 
   public Matrix(int rowCount, int colCount, List<Integer> rows, List<Integer> cols) {
@@ -23,12 +35,28 @@ public class Matrix {
 
   public void setColCount(int colCount) { this.colCount = colCount; }
 
+  public void setMatrix (ArrayList<ArrayList<Integer>> matrix) { this.matrix = matrix; }
+
   public int getRowCount() { return this.rowCount; }
 
   public int getColCount() { return this.colCount; }
 
+  public boolean updateMatrix(int rowCount, int colCount, ArrayList<ArrayList<Integer>> matrix) {
+    setRowCount(rowCount);
+    setColCount(colCount);
+    setMatrix(matrix);
+    return true;
+  }
+
   @Override
   public String toString() {
-    return "";    
+    if(matrix == null) return null;
+
+    String matrixString = "\n";
+    for(ArrayList<Integer> list : matrix) {
+      matrixString += String.valueOf(list) + "\n";
+    }
+
+    return matrixString; 
   }
 }
--- src/test/java/com/aleshamray/rref/MatrixTest.java
+++ src/test/java/com/aleshamray/rref/MatrixTest.java
@@ -1,17 +1,12 @@
 package com.aleshamray.rref;
 
-import org.junit.Test;
 import static org.assertj.core.api.Assertions.*;
+import org.junit.Test;
 
 public class MatrixTest
 {
   @Test
-  public void Matrx_Created_With_3_Rows_and_4_Columns() {
-    final Matrix matrix = new Matrix(3, 4);
+  public void Test_Matrix_Creation() {
 
-    assertThat(matrix.getRowCount()).as("checkk that the row count equals 3")
-                                    .isEqualTo(3);
-    assertThat(matrix.getColCount()).as("check that the column count equals 4")
-                                    .isEqualTo(5);
   }
 }
